00d68649d1a5a4cc0596d0df101f6377306962cd
coumbole <ville.kumpulainen@gmail.com>
2017-11-06 18:58:05 +0200

    Fetches and decodes the messages properly

--- mailscanner/mailscanner.py
+++ mailscanner/mailscanner.py
@@ -36,7 +36,7 @@ def open_connection(verbose=False):
 
 # Param: email.message.Message object
 # Returns: string containing the message body
-def decode_message_body(msg):
+def get_body(msg):
     body = ""
     if msg.is_multipart():
         for part in msg.walk():
@@ -54,38 +54,63 @@ def decode_message_body(msg):
     return body.decode('UTF-8')
 
 
+# Extracts subject line from Message object
+def get_subject(msg):
+    text, encoding = decode_header(msg['subject'])[-1]
+    # Text might be encoded, try to decode
+    try:
+        text = text.decode('UTF-8')
+    # If it's already decoded, pass
+    except AttributeError:
+        pass
+    return text
+
+
 """
     Params:
         conn        IMAP4_SSL connection
         directory   The IMAP directory to look for
         readonly    readonly mode, true or false
+    Returns:
+        List of subject-body tuples
 """
 def fetch_all_messages(conn, directory, readonly):
+
+    # Selected given directory from given connection
     conn.select(directory, readonly)
 
+    # Initialize an empty list to hold the tuples
+    message_data = []
+
+    # Search all messages
     typ, data = conn.search(None, 'All')
+
+    # Loop through each message object
     for num in data[0].split():
+
+        # Fetch message data
         typ, data = conn.fetch(num, '(RFC822)')
-        parse_imap_response(data)
 
-#for header in ['subject', 'to', 'from']:
-#    text, encoding = decode_header(msg[header])[0]
-#    print('{:^8}: {}'.format(
-#        header.upper(), text))
+        # Go through response parts in the messsage data
+        for response_part in data:
 
+            # When a tuple is hit, parse the subject and body
+            if isinstance(response_part, tuple):
 
+                # Read the message using emailparser
+                email_parser = email.parser.BytesFeedParser()
+                email_parser.feed(response_part[1])
+                msg = email_parser.close()
 
-def parse_imap_response(data):
-    bodies = {}
-    for response_part in data:
-        if isinstance(response_part, tuple):
-            email_parser = email.parser.BytesFeedParser()
-            email_parser.feed(response_part[1])
-            msg = email_parser.close()
-            body = decode_message_body(msg)
-            bodies
-            print(body)
+                # Extract body and subject
+                body = get_body(msg)
+                subject = get_subject(msg)
 
+                # Add the data to the list
+                message_data.append((subject, body))
+
+    # Return the list of tuples
+    return message_data
 
 #imaplib.Debug = 4
 if __name__ == '__main__':
