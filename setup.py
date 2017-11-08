7916c456f2a1d90d85dbf729ff8d7ed5426753ae
coumbole <ville.kumpulainen@gmail.com>
2017-11-08 09:27:22 +0200

    Added manifest and updated setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ setup(
     name='mailscanner',
     packages=['mailscanner'],
     version='1.0.0',
-    description='Parses email messages using IMAP',
+    description='Reads and parses email messages using IMAP',
     author='Ville Kumpulainen',
     author_email='ville.kumpulainen@gmail.com',
     url='http://github.com/coumbole/mailscanner',
     keywords = ['email', 'imap', 'parser'],
+    license = 'GPL-3.0',
+    python_requires='>=3',
     classifiers = [
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
         'Inteded Audience :: Developers',
         'License :: GNU General Public License v.3.0 (GPL-3.0)'
